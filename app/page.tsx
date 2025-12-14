aa5b5f5cc526b9e5675e957a07c5b3eba7c3110e
janaccc <jantopler25@gmail.com>
2025-12-14 21:26:09 +0100

    navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { GoSearch } from "react-icons/go";
 import { MdHomeFilled } from "react-icons/md";
+import Navbar from "@/components/Navbar"
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <nav className="h-15 flex justify-between items-center px-6 fixed top-0 left-0 w-full bg-black z-100">
-        <div className="flex gap-6 items-center">
-          <Image src="/images/zvokifylogo.png" alt="logo" width={35} height={35}/>
-          <Link href="/" className="bg-background w-11 h-11 grid place-items-center text-white rounded-full"> 
-          <MdHomeFilled size={20}/>
-          </Link>
-            <div className="bg-background flex items-center h-11 w-90 px-3 gap-3 text-primary-text rounded-full">
-              <GoSearch className="text-primary-text shrink-0"/>
-              <input className="height-full w-full outline-none placeholder:text-primary-text" type="text" placeholder="Išči skladbe..."/>
-            </div>
-        </div>
-      </nav>
+        <Navbar/>
     </div>
   );
 }
+
---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import { GoSearch } from "react-icons/go";
+import { MdHomeFilled } from "react-icons/md";
+
+function Navbar() {
+    return (
+        <nav className="h-15 flex justify-between items-center px-6 fixed top-0 left-0 w-full bg-black z-100">
+            <div className="flex gap-6 items-center">
+                <Image src="/images/zvokifylogo.png" alt="logo" width={35} height={35} />
+                <Link href="/" className="bg-background w-11 h-11 grid place-items-center text-white rounded-full">
+                    <MdHomeFilled size={20} />
+                </Link>
+                <div className="bg-background flex items-center h-11 w-90 px-3 gap-3 text-primary-text rounded-full">
+                    <GoSearch className="text-primary-text shrink-0" />
+                    <input className="height-full w-full outline-none placeholder:text-primary-text" type="text" placeholder="Išči skladbe..." />
+                </div>
+            </div>
+            <div className="flex items-center gap-8">
+                <div className="flex gap-2 text-secondary-text font-bold border-r-2 border-primary-text pr-6">
+                    <a href="https://github.com/janaccc" className="hover:text-primary-text">GitHub</a>
+                </div>
+                <div>
+                    <Link href="/login" className="h-11 bg-white text-gray-950 rounded-full font-bold hover:bg-secondary-text grid px-8 place-items-center">Prijava</Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
