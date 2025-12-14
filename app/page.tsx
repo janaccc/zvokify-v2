5258b258b5b222f175321190413aef75073c1e39
janaccc <jantopler25@gmail.com>
2025-12-14 19:56:03 +0100

    globals, css, icon...

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <nav className="h-15 flex justify-between items-center px-6 fixed top-0 left-0 w-full bg-black z-100">
+
+      </nav>
+    </div>
+  );
+}
